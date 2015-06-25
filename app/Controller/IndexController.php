1645c1fd67e0a1b4d2159f43173f5aff02349bfa
Loy <loyenrique1@gmail.com>
2015-06-24 22:15:45 -0430

    first commit

---
+++ app/Controller/IndexController.php
@@ -0,0 +1,19 @@
+<?php
+ 
+class IndexController extends AppController {
+    public $helpers = array ('Html','Form');
+
+    function index() {
+        // $this->set('posts', $this->Post->find('all'));
+    }
+	
+	function prueba() {
+        
+    }
+	
+	function hola() {
+        
+    }
+}
+
+?>
---
+++ app/View/Posts/index.ctp
@@ -0,0 +1,5 @@
+Hola yo soy index de post controller
+
+<?php 
+	debug($posts);
+?>
